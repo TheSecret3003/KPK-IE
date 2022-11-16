a9879a2189d7e7bedded2955a10ecc3d681722a5
unknown <23522048@std.stei.itb.ac.id>
2022-11-16 07:12:09 +0700

    test search func

--- indexing/main.py
+++ indexing/main.py
@@ -3,9 +3,9 @@ import pandas as pd
 
 def search_test(nama_instansi):
     # Read indexing table
-    index_table = pd.read_csv('https://raw.githubusercontent.com/intanq/data/main/index_table.csv')
+    index_table = pd.read_csv('data\index_table.csv')
     # Read reference data
-    reference_data = pd.read_csv('https://raw.githubusercontent.com/intanq/data/main/index_table.csv')
+    reference_data = pd.read_csv('data\\reference_data.csv')
 
     search_ = search.Search(reference_data, index_table)
     phrase_candidates = search_.search(nama_instansi)
