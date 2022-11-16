f99eeccfa2aecfc0ca4450b3ec06ed9cd1f29bcd
unknown <23522048@std.stei.itb.ac.id>
2022-11-16 17:13:21 +0700

    add new api - get references

--- indexing/app.py
+++ indexing/app.py
@@ -3,21 +3,17 @@ import pandas as pd
 import numpy as np
 import search
 
-"""
-raise HTTPError(req.full_url, code, msg, hdrs, fp)
-urllib.error.HTTPError: HTTP Error 404: Not Found
-"""
-
 app = Flask(__name__)
 
 @app.route('/get-references', methods=['GET'])
 def get_references():
     # Read indexing table
-    index_table = pd.read_csv('https://raw.githubusercontent.com/intanq/data/main/index_table.csv')
+    index_table = pd.read_csv('./data/v2/index_table.csv')
     # Read reference data
-    reference_data = pd.read_csv('https://raw.githubusercontent.com/intanq/data/main/index_table.csv')
-
-    nama_instansi = request.args.get('nama_instansi')
+    reference_data = pd.read_csv('./data/v2/reference_data.csv')
+    
+    args = request.args
+    nama_instansi = args.get('nama_instansi')
 
     search_ = search.Search(reference_data, index_table)
     phrase_candidates = search_.search(nama_instansi)
