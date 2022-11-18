cffcf6c112d8d25bb4781b08d8abae928fc876b3
unknown <23522048@std.stei.itb.ac.id>
2022-11-18 23:17:59 +0700

    use dynamic threshold

--- string_distance_levenshtein/get_candidates.py
+++ string_distance_levenshtein/get_candidates.py
@@ -2,30 +2,32 @@ from levenshtein import levenshtein
 from search_utils import search
 import itertools
 
-def get_candidates(nama_instansi, threshold=15):
+def get_candidates(nama_instansi):
     """
     Function to get similar phrase candidates that have an edit distance 
     that is less than or equal passed edit distance threshold
     Args:
     - nama_instansi: str -> name of the instansi to be looked up
-    - threshold: int -> edit distance threshold
     Returns:
-    - top5_phrases: dict -> dictionary of phrases and its edit distance (minimum)
+    - min_dist_phrases: dict -> dictionary of phrases and its edit distance (minimum)
     """
     phrase_candidates = search(nama_instansi)
     similar_phrases_dict = {}
 
     for word, candidates in phrase_candidates.items():
         for cand in candidates:
+            # Define threshold
+            threshold = 0
+            if len(nama_instansi) < len(cand):
+                threshold = len(cand) - len(nama_instansi)
+            elif len(nama_instansi) > len(cand):
+                threshold = len(nama_instansi) - len(cand)
+
             edit_dist = levenshtein(nama_instansi, cand)
             if edit_dist <= threshold:
                 similar_phrases_dict[cand] = edit_dist
 
     sorted_by_edit_dist = dict(sorted(similar_phrases_dict.items(), key=lambda item: item[1]))
-    top5_phrases = dict(itertools.islice(sorted_by_edit_dist.items(), 5))
+    min_dist_phrases = dict(itertools.islice(sorted_by_edit_dist.items(), 5))
     
-    return top5_phrases
-
-
-# print(len(get_candidates('dewan ketahanan')))
-# print(get_candidates('dewan ketahanan'))
+    return min_dist_phrases
--- string_distance_levenshtein/main.py
+++ string_distance_levenshtein/main.py
@@ -1,10 +1,14 @@
 from get_candidates import get_candidates
 
 if __name__ == "__main__":
-    print(get_candidates("garuda indonesia"))
+    print("kai: ", get_candidates("kai"))
+    print("pelni: ", get_candidates("pelni"))
+    print("banyumas: ", get_candidates("banyumas"))
     
     """
     Sample output:
-    {'garuda indonesia': 0, 'pt garuda indonesia': 3, 'barata indonesia': 3, 'pal indonesia': 5, 'pupuk indonesia': 5}
+    kai:  {'pt kai': 3, 'pt kereta api indonesia persero kai': 32}
+    pelni:  {'pt pelni': 3, 'pt pelayaran nasional indonesia persero pelni': 40}
+    banyumas:  {'pemkab banyumas': 7, 'pemerintah banyumas': 11, 'pemerintah kabupaten banyumas': 21}
     """
     
